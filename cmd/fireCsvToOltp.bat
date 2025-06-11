61ef271abd85fa29cc30e70c660816fbca994016
Mihail Komarov <michaelkomarov03@gmail.com>
2025-06-11 16:12:16 +0300

    fix fireCsvToOltp.bat

--- cmd/fireCsvToOltp.bat
+++ cmd/fireCsvToOltp.bat
@@ -1,6 +1,4 @@
-docker exec -e ^
-PGPASSWORD="oltp_password" ^
-oltp ^
-bash -c ^
-'for f in /opt/etl_scripts/*.sql; ^
-do psql -h localhost -U oltp_user -d oltp_db -f "$f" || exit 1; done'
+docker exec ^
+  -e PGPASSWORD="oltp_password" ^
+  oltp ^
+  bash -c "for f in /opt/etl_scripts/*.sql; do psql -h localhost -U oltp_user -d oltp_db -f \"$f\" || exit 1; done"
