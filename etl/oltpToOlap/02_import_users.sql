ca3e62e833d732008efeeec945283880718e6131
Mihail Komarov <michaelkomarov03@gmail.com>
2025-06-14 16:21:41 +0300

    remove view for users import

--- etl/oltpToOlap/02_import_users.sql
+++ etl/oltpToOlap/02_import_users.sql
@@ -1,13 +1,16 @@
-CREATE OR REPLACE VIEW v_dim_user AS
-SELECT id AS user_id,
-       email,
-       display_name
-FROM oltp_users;
+DROP FOREIGN TABLE IF EXISTS oltp_users CASCADE;
+CREATE FOREIGN TABLE oltp_users (
+    id BIGINT,
+    email TEXT,
+    display_name TEXT
+    )
+    SERVER oltp_server
+    OPTIONS (schema_name 'public', table_name 'users');
 
 INSERT INTO dim_user (user_id, email, display_name)
-SELECT vdu.user_id,
-       vdu.email,
-       vdu.display_name
-FROM v_dim_user vdu
+SELECT ou.id AS user_id,
+       ou.email,
+       ou.display_name
+FROM oltp_users ou
 ON CONFLICT (user_id) DO UPDATE SET email        = EXCLUDED.email,
                                     display_name = EXCLUDED.display_name;
